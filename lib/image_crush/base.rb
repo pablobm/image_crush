e9a785b898c8f868587f96ed8bda4c9245e75160
Pablo Brasero Moreno <pablobm@gmail.com>
2009-07-05 21:52:55 +0100

    Moving code around.

---
+++ lib/image_crush/base.rb
@@ -0,0 +1,57 @@
+module ImageCrush
+
+  def self.crush(path)
+    raise ImageCrush::InputFileNotFound unless File.exist?(path)
+
+    if File.file?(path)
+      crush_file(path)
+    elsif File.directory?(path)
+      crush_dir(path)
+    else
+      raise "I don't know what to do with #{path}"
+    end
+  end
+
+  def self.crush_file(path)
+    processor = select_processor(path) or return nil
+    source_path = copy_to_tempdir(path)
+    crushed_path = source_path + '.crushed'
+    processor.crush(source_path, crushed_path)
+    FileUtils.cp(crushed_path, path) if File.exist?(crushed_path)
+  end
+
+  def self.crush_dir(path)
+    Dir.foreach(path) do |entry|
+      next if %w{. ..}.include?(entry)
+      crush(File.join(path, entry))
+    end
+  end
+
+  def self.select_processor(path)
+    case path
+    when /\.png$/i
+      Pngcrush
+    when /\.jpe?g$/i
+      Jpegtran
+    end
+  end
+
+  def self.copy_to_tempdir(path)
+    wdir = tmpdir
+    FileUtils.cp_r path, wdir
+    File.join(wdir, File.basename(path))
+  end
+
+  def self.tmpdir
+    ret = File.join(Dir.tmpdir, 'image_crush')
+    FileUtils.mkdir_p(ret)
+    ret
+  end
+
+  class CrushToolNotAvailable < StandardError
+  end
+
+  class InputFileNotFound < StandardError
+  end
+
+end
