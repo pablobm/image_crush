07e8b7c77184727b3f8382fd2c42f82c6f50b35b
Pablo Brasero Moreno <pablobm@gmail.com>
2009-07-05 17:12:53 +0100

    Leave recursion to ImageCrush.

--- lib/image_crush.rb
+++ lib/image_crush.rb
@@ -6,7 +6,24 @@ module ImageCrush
     raise ImageCrush::InputFileNotFound unless File.exist?(path)
     raise ImageCrush::CrushToolNotAvailable unless Pngcrush.available?
 
+    if File.file?(path)
+      crush_file(path)
+    elsif File.directory?(path)
+      crush_dir(path)
+    else
+      raise "I don't know what to do with #{path}"
+    end
+  end
+
+  def self.crush_file(path)
     Pngcrush.crush(path)
+  end
+
+  def self.crush_dir(path)
+    Dir.foreach(path) do |entry|
+      next if %w{. ..}.include?(entry)
+      crush(File.join(path, entry))
+    end
   end
 
   class CrushToolNotAvailable < StandardError
--- lib/image_crush/pngcrush.rb
+++ lib/image_crush/pngcrush.rb
@@ -4,25 +4,8 @@ module ImageCrush
     def self.available?
       ! %x{which pngcrush}.empty?
     end
-    
+
     def self.crush(path)
-      if File.file?(path)
-        self.crush_file(path)
-      elsif File.directory?(path)
-        self.crush_dir(path)
-      else
-        raise "I don't know what to do with #{path}"
-      end
-    end
-    
-    def self.crush_dir(path)
-      Dir.foreach(path) do |entry|
-        next if %w{. ..}.include?(entry)
-        crush(File.join(path, entry))
-      end
-    end
-    
-    def self.crush_file(path)
       wdir = tmpdir
       FileUtils.cp_r path, wdir
       png_path = File.join(wdir, File.basename(path))
@@ -30,7 +13,7 @@ module ImageCrush
       %x{pngcrush #{png_path} #{crushed_path}}
       FileUtils.cp(crushed_path, path) if File.exist?(crushed_path)
     end
-    
+
     def self.tmpdir
       ret = File.join(Dir.tmpdir, 'pngcrush')
       FileUtils.mkdir_p(ret)
