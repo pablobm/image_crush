bc54b5fc21f57a333bbcb82f50b62b697520f598
Pablo Brasero Moreno <pablobm@gmail.com>
2009-07-05 21:56:44 +0100

    More tidying up.

--- lib/image_crush.rb
+++ lib/image_crush.rb
@@ -1,9 +1,11 @@
-require 'image_crush/base'
-require 'image_crush/pngcrush'
-require 'image_crush/jpegtran'
-
 module ImageCrush
+  LIB_ROOT = File.dirname(__FILE__)
 end
+
+base_dir = File.join(ImageCrush::LIB_ROOT, 'image_crush')
+require File.join(base_dir, 'base')
+require File.join(base_dir, 'pngcrush')
+require File.join(base_dir, 'jpegtran')
 
 def ImageCrush(path)
   ImageCrush.crush(path)
