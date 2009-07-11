ef6dbcab2cea764a2acfd069be1c1cea7d02dc6a
Pablo Brasero Moreno <pablobm@gmail.com>
2009-07-11 01:40:16 +0100

    Decorator module to add messages.

--- spec/image_crush_spec.rb
+++ spec/image_crush_spec.rb
@@ -5,12 +5,6 @@ require 'fileutils'
 
 describe ImageCrush do
 
-  FIXTURES_ROOT = File.join(File.dirname(__FILE__), 'fixtures')
-  DICE_PATH = File.join(FIXTURES_ROOT, 'dice.png')
-  CCBY_PATH = File.join(FIXTURES_ROOT, 'subdir', 'cc-by-icon.png')
-  MMAN_PATH = File.join(FIXTURES_ROOT, 'happy-mailman.jpg')
-  README_PATH = File.join(FIXTURES_ROOT, 'README.txt')
-
   before :each do
     remove_any_work_dirs
   end
@@ -49,12 +43,6 @@ describe ImageCrush do
 
 
     private
-
-    def copy_file_to_tmpdir(path)
-      create_work_dir
-      FileUtils.cp path, @work_dir
-      File.join(@work_dir, path.gsub(FIXTURES_ROOT, ''))
-    end
 
     def should_reduce_size_of_image(path)
       f = File.open(path)
@@ -99,15 +87,5 @@ describe ImageCrush do
     end
   end
 
-
-  private
-
-  def remove_any_work_dirs
-    Dir.glob(File.join(Dir.tmpdir, 'image_crush.spec.*')).each{|path| FileUtils.rm_rf(path)}
-  end
-
-  def create_work_dir
-    @work_dir = File.join(Dir.tmpdir, 'image_crush.spec.' + $$.to_s)
-    FileUtils.mkdir_p(@work_dir)
-  end
 end
+
